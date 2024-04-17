e85fa44779acf6161e6ecbe5250dd7f003b5e178
Raj Pandey <rajpandey552002@gmail.com>
2024-04-17 20:18:25 +0530

    rendering map

--- script.js
+++ script.js
@@ -15,6 +15,33 @@ if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
         const {latitude} = position.coords;
         const {longitude} = position.coords;
+
+        const coords = [latitude, longitude];
+
+        const map = L.map('map').setView(coords, 15);
+
+L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+
+    map.on('click', function(mapEvent){
+        console.log(mapEvent);
+
+        const {lat, lng} = mapEvent.latlng;
+
+        L.marker([lat,lng]).addTo(map)
+    .bindPopup(L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+    })).setPopupContent('Workout')
+    .openPopup();
+    });
+
     }, function(){
         alert('Could not get your position');
     });
